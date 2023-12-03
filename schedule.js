25630bd98a59804e803038df78d93a4308703e30
Rubén Martínez González <rubenmartinezgonzalez94@gmail.com>
2023-12-02 20:18:12 -0600

    add html table to show schedule

--- schedule.js
+++ schedule.js
@@ -36,11 +36,11 @@ class ScheduleGenerator {
         displayOptimalSchedule(timetable);
     }
 
-    // Método para encontrar un espacio disponible en el horario para una asignatura (Orientado a Objetos)
+// Método para encontrar un espacio disponible en el horario para una asignatura (Orientado a Objetos)
     findAvailableSlot(timetable, subject) {
         for (let day = 0; day < 5; day++) { // Supongamos una semana de lunes a viernes
             let dailyClasses = 0; // Contador para el número diario de clases
-            for (let hour = 8; hour < 18 - subject.duration + 1; hour++) { // Solo de 8 AM a 6 PM
+            for (let hour = 8; hour < 17 - subject.duration + 1; hour++) { // Modificamos el límite a 5 PM
                 if (!this.isSlotOccupied(timetable, day, hour, subject.duration, subject.frequency)) {
                     dailyClasses++;
                     if (dailyClasses === 2) { // Limitar a 2 clases por día
@@ -54,18 +54,18 @@ class ScheduleGenerator {
         return -1;
     }
 
-
-    // Método para verificar si un espacio en el horario está ocupado
+// Método para verificar si un espacio en el horario está ocupado
     isSlotOccupied(timetable, day, hour, duration, frequency) {
         for (let i = 0; i < duration; i++) {
             for (let j = 0; j < frequency; j++) {
-                if (timetable[day + j] && timetable[day + j][hour + i]) {
+                if (timetable[day + j] && timetable[day + j][hour + i] && (hour + i >= 8 && hour + i < 17)) {
                     return true;
                 }
             }
         }
         return false;
     }
+
 
     // Método para programar una asignatura en el horario
     scheduleClass(timetable, subject, slot) {
@@ -103,6 +103,19 @@ function displaySubject(subject) {
     const scheduleList = document.getElementById('scheduleList');
     const listItem = document.createElement('li');
     listItem.textContent = `${subject.name} - Profesor: ${subject.professor} - Duración: ${subject.duration} horas - Frecuencia: ${subject.frequency} veces por semana`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Eliminar';
+    deleteButton.onclick = function () {
+        // Lógica para eliminar la asignatura
+        const index = scheduleGenerator.schedule.indexOf(subject);
+        if (index !== -1) {
+            scheduleGenerator.schedule.splice(index, 1);
+            listItem.remove();
+        }
+    };
+
+    listItem.appendChild(deleteButton);
     scheduleList.appendChild(listItem);
 }
 
@@ -112,19 +125,41 @@ function generateOptimalSchedule() {
 }
 
 // Función para mostrar el horario óptimo en la interfaz (Paradigma Funcional)
+// Función para mostrar el horario óptimo en la interfaz (Paradigma Funcional)
 function displayOptimalSchedule(timetable) {
     const scheduleList = document.getElementById('scheduleList');
-    scheduleList.innerHTML = "";
+    const timetableChart = document.getElementById('timetableChart');
+    timetableChart.innerHTML = ""; // Limpiamos el contenido anterior
 
-    timetable.forEach((day, dayIndex) => {
-        day.forEach((subject, hourIndex) => {
-            if (subject) {
-                const listItem = document.createElement('li');
-                listItem.textContent = `${subject} - Día: ${dayIndex + 1}, Hora: ${hourIndex + 8}`;
-                scheduleList.appendChild(listItem);
-            }
-        });
+    // Crea una tabla para representar el horario como un diagrama de Gantt
+    const table = document.createElement('table');
+    const daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']; // Puedes extender para más días
+
+    // Crea la primera fila con los días de la semana
+    let headerRow = '<tr><th></th>';
+    daysOfWeek.forEach(day => {
+        headerRow += `<th>${day}</th>`;
     });
+    headerRow += '</tr>';
+    table.innerHTML += headerRow;
+
+    // Crea las filas para cada hora y asignatura en el horario
+    for (let hour = 8; hour <= 17; hour++) {
+        let row = `<tr><td>${hour}:00</td>`;
+        for (let day = 0; day < 5; day++) {
+            if (timetable[day] && timetable[day][hour]) {
+                const subject = timetable[day][hour];
+                row += `<td>${subject}</td>`;
+            } else {
+                row += '<td></td>';
+            }
+        }
+        row += '</tr>';
+        table.innerHTML += row;
+    }
+
+    // Agrega la tabla al contenedor del diagrama de Gantt
+    timetableChart.appendChild(table);
 }
 
 // Ejemplo de uso
